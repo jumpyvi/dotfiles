125de04a21dfe23170532ed0ccf251a7d8d19303
JumpyVi <jumpy634@gmail.com>
2025-01-09 12:01:21 -0500

    Update .config/fish/config.fish

--- dot_config/private_fish/config.fish
+++ dot_config/private_fish/config.fish
@@ -1,11 +1,8 @@
 if status is-interactive
-    bind \b backward-kill-word
-end
-eval (/home/linuxbrew/.linuxbrew/bin/brew shellenv)
-set -U fish_greeting
-abbr -a vi nvim
-abbr -a dk docker
-alias cd="z"
 
-starship init fish | source
-zoxide init fish | source
+end
+
+set -U fish_greeting
+
+
+
